1628b6a9f439eee8da1a91beba0f307e3899e8d5
André König <andre.koenig@gmail.com>
2012-08-11 16:17:01 +0200

    Added first structure.

---
+++ 20120813/memory-game/javascripts/Cards.js
@@ -0,0 +1,21 @@
+/*!
+ * Bremen.js
+ *
+ * Copyright(c) 2012 Bremen, Germany
+ *
+ * Authors:
+ *
+ *     Lukas Magedanz <lukas.magedanz@gmail.com>
+ *     André König <andre.koenig@gmail.com>
+ *
+ * MIT Licensed
+ *
+ */
+var Cards = {
+
+    {
+        id: 'card-01',
+        imageUrl: 'http://barcampcologne.mixxt.de/storage/images/profiles/d/5/4/0ec4cb4c1536273bdf869ce8bcc72.jpg'
+    }
+
+};
---
+++ 20120813/memory-game/javascripts/Player.js
@@ -0,0 +1,21 @@
+var Player = function () {
+
+    var name
+      , score;
+
+    this.getName = function () {
+        return name;
+    };
+
+    this.setName = function (newName) {
+        name = newName;
+    };
+
+    this.getScore = function () {
+        return score;
+    };
+
+    this.setScore = function () {
+
+    };
+};
